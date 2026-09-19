21bb9202df626b1cf7bfddc99dd36b1057c578d1
shuvobasak1998 <shuvobasak01752@gmail.com>
2026-09-19 23:38:48 +0600

    lesson 5 doc and  practice done

---
+++ lesson_practice_5.js
@@ -0,0 +1,71 @@
+//Exercise 1
+const subject = [
+    'Laravel',
+    'React',
+    'Node.js',
+    'NestJS',
+]
+
+console.log(subject[subject.length-subject.length],subject[(subject.length-1)])
+
+
+
+//Exercise 2
+const numbers = [10, 20, 30];
+numbers.push(40);
+console.log(numbers);
+
+
+
+//Exercise 3
+const fruits = [
+    "Apple",
+    "Banana",
+    "Mango"
+];
+
+console.log(fruits[1]);
+console.log(fruits.length);
+
+
+
+//Exercise 4
+const users = [
+    {
+        id: 1,
+        name: "Shuvo"
+    },
+    {
+        id: 2,
+        name: "Rahim"
+    },
+    {
+        id: 3,
+        name: "Karim"
+    }
+];
+
+console.log(users[0].name);
+console.log(users[2].name);
+
+
+
+
+//Exercise 5
+const cart = [
+    {
+        name: "Chair",
+        price: 5000,
+        quantity: 2
+    },
+    {
+        name: "Table",
+        price: 8000,
+        quantity: 1
+    }
+];
+
+console.log(cart[0].price);
+console.log(cart[1].quantity);
+console.log(cart.length);
+
